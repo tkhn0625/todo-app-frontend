fdf8211591a7037498adeac89b883e019de81370
tkhn0625 <athleta.0315@gmail.com>
2021-01-06 19:07:11 +0900

    decorate component

--- src/components/todo/ToDoApp.jsx
+++ src/components/todo/ToDoApp.jsx
@@ -7,18 +7,51 @@ class ToDoApp extends Component{
     return(
       <div className="TodoApp">
         <Router>
+          <HeaderComponent/>
           {/* URIをチェックして、アクション内容を切り替える。 */}
-          <Switch>
-            <Route path="/" exact component={LoginComponent}/>
-            <Route path="/login" component={LoginComponent}/>
-            {/* :name　任意の値を設定できる。 */}
-            <Route path="/welcome/:name" component={WelcomeComponent}/>
-            <Route path="/todos" component={ListTodosComponent}/>
-            {/* 定義したURI以外の場合のコンポーネント起動 */}
-            <Route component={ErrorComponent}/>
-          </Switch>
+            <Switch>
+              <Route path="/" exact component={LoginComponent}/>
+              <Route path="/login" component={LoginComponent}/>
+              {/* :name　任意の値を設定できる。 */}
+              <Route path="/welcome/:name" component={WelcomeComponent}/>
+              <Route path="/todos" component={ListTodosComponent}/>
+              <Route path="/logout" component={LogoutComponent}/>
+              {/* 定義したURI以外の場合のコンポーネント起動 */}
+              <Route component={ErrorComponent}/>
+            </Switch>
+          <FooterComponent/>
         </Router>
       </div>
+    )
+  }
+}
+
+class HeaderComponent extends Component{
+  render(){
+    return(
+      <header>
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+          <div><a href="http://www.tkhn0625.com" className="navbar-brand">tkhn0625</a></div>
+          <ul className="navbar-nav">
+            <li><Link className="nav-link" to="/welcome/tkhn0625">Home</Link></li>
+            <li><Link className="nav-link" to="/todos">Todos</Link></li>
+          </ul>
+          <ul className="navbar-nav navbar-collapse justify-content-end">
+            <li><Link className="nav-link" to="/login">Login</Link></li>
+            <li><Link className="nav-link" to="/logout">Logout</Link></li>
+          </ul>
+        </nav>
+      </header>
+    )
+  }
+}
+
+class FooterComponent extends Component{
+  render(){
+    return(
+      <footer className="fotter">
+        <span className="text-muted">All Rights Reserved 2021 @tkhn0625</span>
+      </footer>
     )
   }
 }
@@ -40,27 +73,27 @@ class ListTodosComponent extends Component{
     return(
       <div>
         <h1>List Todos</h1>
-        <table>
-            <tr>
-              <th>id</th>
-              <th>description</th>
-              <th>Is Completed?</th>
-              <th>Target Date</th>
-            </tr>
-          <tbody>
-            {
-              this.state.todos.map (
-                todo =>
-                  <tr>
-                    <td>{todo.id}</td>
-                    <td>{todo.description}</td>
-                    <td>{todo.done.toString()}</td>
-                    <td>{todo.targetDate.toString()}</td>
-                  </tr>
-              )
-            }
-          </tbody>
-        </table>
+        <div className="container">
+          <table className="table">
+              <tr>
+                <th>description</th>
+                <th>Is Completed?</th>
+                <th>Target Date</th>
+              </tr>
+            <tbody>
+              {
+                this.state.todos.map (
+                  todo =>
+                    <tr>
+                      <td>{todo.description}</td>
+                      <td>{todo.done.toString()}</td>
+                      <td>{todo.targetDate.toString()}</td>
+                    </tr>
+                )
+              }
+            </tbody>
+          </table>
+        </div>
       </div>
     )
   }
@@ -70,7 +103,10 @@ class WelcomeComponent extends Component{
   render(){
     return(
       <div>
-        Welcome {this.props.match.params.name}. You can manage your todos <Link to="/todos">here</Link>.
+        <h1>Welcome!</h1>
+        <div className="container">
+          Welcome {this.props.match.params.name}. You can manage your todos <Link to="/todos">here</Link>.
+        </div>
       </div>
     )
   }
@@ -78,6 +114,19 @@ class WelcomeComponent extends Component{
 
 function ErrorComponent(){
   return <div>An Error Occurred. I don't know what to do ! Contact support at abcd@gmail.com</div>
+}
+
+class LogoutComponent extends Component{
+  render(){
+    return(
+      <div>
+        <h1>You are logged out</h1>
+        <div className="container">
+          Thank You for Using Our Application.
+        </div>
+      </div>
+    )
+  }
 }
 
 class LoginComponent extends Component{
@@ -112,14 +161,14 @@ class LoginComponent extends Component{
   render(){
     return(
       <div>
-        {/* <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed}/> */}
-        {/* <ShowLoginSuccessfull showSuccessfulMessages={this.state.showSuccessfulMessages}/> */}
-        {/* boolean && 動作内容 */}
-        {this.state.hasLoginFailed && <div>Invalid Credentials</div>}
-        {/* {this.state.showSuccessfulMessages && <div>Login Successfull</div>} */}
-        User Name: <input type="text" name ="username" value={this.state.username} onChange={this.handleChange}/>
-        Password: <input type="password" name ="password" value={this.state.password} onChange={this.handleChange}/>
-        <button　onClick={this.loginClicked}>Login</button>
+        <h1>Login</h1>
+        <div className="container">
+          {/* boolean && 動作内容 */}
+          {this.state.hasLoginFailed && <div className="alert alert-warning">Invalid Credentials</div>}
+          User Name: <input type="text" name ="username" value={this.state.username} onChange={this.handleChange}/>
+          Password: <input type="password" name ="password" value={this.state.password} onChange={this.handleChange}/>
+          <button onClick={this.loginClicked}>Login</button>
+        </div>
       </div>
     )
   }
